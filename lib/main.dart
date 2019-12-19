948de00ea8ebe7e3ead27ad3d63671c56e820b06
lisboaa <douglas.lisboa@edu.unipar.br>
2019-12-19 00:56:21 -0300

    Implementando button de envio parte 1.

--- lib/main.dart
+++ lib/main.dart
@@ -16,37 +16,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Lista de mcado'),
-            ),
-            body: new GridView.count(
-              crossAxisCount: 2,
-              children: <Widget>[
-                new Container(
-                  margin: const EdgeInsets.only(right: 10, left: 10, top: 150),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Infome o nome!',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lista de mcado'),
+        ),
+        body: new GridView.count(
+          crossAxisCount: 2,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(right: 10, left: 10, top: 150),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Infome o nome!',
+                  border: OutlineInputBorder(),
                 ),
-                new Container(
-                  margin: const EdgeInsets.only(
-                    right: 10,
-                    left: 10,
-                    top: 150,
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Informe o valor!',
-                      icon: new Icon(Icons.add_shopping_cart),
-                      border: OutlineInputBorder(),
+              ),
+            ),
+            new Container(
+              margin: const EdgeInsets.only(
+                right: 10,
+                left: 10,
+                top: 150,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Informe o valor!',
+                  icon: new Icon(Icons.add_shopping_cart),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(0.0),
+                    color: Colors.cyanAccent,
+                    width: 100.0,
+                    height: 40.0,
+                    child: RaisedButton(
+                      color: Colors.deepOrange,
+                      child: Text(
+                        'Enviar',
+                      ),
+                      onPressed: () {},
+                      padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                      elevation: 5.0,
                     ),
                   ),
                 ),
               ],
-            )));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
